d90344423594d5ecc1d23c9a7042c6860b9cbf3e
Paul Chung <paulchung39@gmail.com>
2017-04-20 20:46:53 -0700

    replaced bcrypt library

--- app.js
+++ app.js
@@ -56,13 +56,14 @@ app.use(function(err, req, res, next) {
 });
 
 const port = process.env.PORT || 3090;
+const server = http.createServer(app);
 /*
 const server = http.createServer(app);
 server.listen(port);
 console.log("Server listening on: ", port);
 */
 
-app.listen(port, function() {
+server.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
 
